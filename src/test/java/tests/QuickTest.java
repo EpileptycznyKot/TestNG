0c2ea3f76c37afd8c23218cbc617135621ef3ab0
Krzysztof <krzysztof.klopotowski@protonmail.com>
2022-05-19 19:27:41 +0200

    Initial commit.

---
+++ src/test/java/tests/QuickTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class QuickTest {
+
+    @BeforeClass
+    public static void setup(){
+
+    }
+
+
+    @Test
+    public void test1(){
+
+    }
+}
